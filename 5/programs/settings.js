fc0b898bf014dfe5691f5ff7c0f0792a924d14ed
dot dot goose <138257284+6d6t6@users.noreply.github.com>
2024-11-13 16:54:58 -0700

    Add files via upload

---
+++ 5/programs/settings.js
@@ -0,0 +1,5 @@
+function openSettings() {
+    const content = document.createElement("div");
+    content.innerHTML = "<h1>Settings</h1><p>Here you can adjust your application settings.</p>";
+    return content;
+} 
